0c56fdb68e8d2e7ab0db11e3f3e2bd8695e9176b
Thiago Brauer Ribeiro <thiagobrauer@gmail.com>
2018-06-01 15:17:26 -0300

    Disable Login's page autocomplete only when 'APP_ENV' is set to 'production'

--- src/views/io/auth/form-login.blade.php
+++ src/views/io/auth/form-login.blade.php
@@ -1,11 +1,11 @@
 <form action="{{ route('signin') }}" method="post" id="login_form" autocomplete="off">
   <div class="form-group">
     <label for="email" class="bmd-label-floating">Email</label>
-    <input autocomplete="off" type="email" class="form-control form-control-lg" id="email" name = 'email'>
+  <input autocomplete="@if(env('APP_ENV') == 'production') {{'off'}} @else {{ 'on' }} @endif" type="email" class="form-control form-control-lg" id="email" name = 'email'>
   </div>
   <div class="form-group bmd-form-group">
     <label for="password" class="bmd-label-floating">Senha</label>
-    <input autocomplete="new-password" type="password" class="form-control form-control-lg" id="password" name = 'password'>
+    <input autocomplete="@if(env('APP_ENV') == 'production') {{'new-password'}} @else {{ 'on' }} @endif" type="password" class="form-control form-control-lg" id="password" name = 'password'>
   </div>
   <br/>
   <div class="checkbox">
