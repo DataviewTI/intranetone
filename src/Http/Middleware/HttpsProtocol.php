22a8f0059af4500cb14d994a270c6c1ee34e91e2
Thiago Brauer Ribeiro <thiagobrauer@gmail.com>
2018-06-05 16:07:36 -0300

    Changed variable env to variable config at middleware HttpsProtocol

--- src/Http/Middleware/HttpsProtocol.php
+++ src/Http/Middleware/HttpsProtocol.php
@@ -8,7 +8,7 @@ class HttpsProtocol {
 
     public function handle($request, Closure $next)
     {
-            if (!$request->secure() && env('APP_ENV') === 'production') {
+            if (!$request->secure() && config('app.env') === 'production') {
                 return redirect()->secure($request->getRequestUri());
             }
 
