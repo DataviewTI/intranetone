0d6718ea5698261f893e4244b9b83a5174a521d4
Thiago Brauer Ribeiro <thiagobrauer@gmail.com>
2018-11-29 15:59:34 -0300

    Update datatables component

--- src/views/io/components/datatable.blade.php
+++ src/views/io/components/datatable.blade.php
@@ -1,4 +1,4 @@
-<table class="table table-striped table-bordered hover" cellspacing="0" width="100%" id="{{$_id}}">
+<table class="table table-striped table-bordered hover  @if(isset($_class)) {{ $_class }} @endif" cellspacing="0" width="100%" id="{{$_id}}">
 	<thead>
 		@foreach($_columns as $col)
 			<th class = "__dt_{{str_slug($col['title'])}}">{!! $col['title'] !!}</th>
